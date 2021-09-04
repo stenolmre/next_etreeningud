5ef3d7631e524ee9db3e4b38bc03fe707561fc40
Sten Olmre <stenolmre@icloud.com>
2021-09-04 21:23:34 +0300

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,14 @@
-import React from 'react'
-
-import GlobalState from './../context/state'
+import React, { Fragment } from 'react'
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import './../css/styles.css'
-import './../css/posts.css'
-import './../css/fitness.css'
-import './../css/landing.css'
-import './../css/admin.css'
-
-import MarkVisitor from './../utils/markvisitor'
 
 const App = ({ Component, pageProps }) => {
-  return <GlobalState>
-    <MarkVisitor>
-      <Component { ...pageProps } />
-      <style jsx>{`
-        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;900&display=swap');
-      `}</style>
-    </MarkVisitor>
-  </GlobalState>
+  return <Fragment>
+    <Component { ...pageProps } />
+    <style jsx>{`
+      @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;900&display=swap');
+    `}</style>
+  </Fragment>
 }
 
 export default App
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 import React, { Fragment } from 'react'
-import Head from './../components/utils/head'
-
-import Layout from './../components/layout'
-import Landing from './../components/landing/landing'
+import Head from 'next/head'
 
 const Index = () => {
   return <Fragment>
-    <Head title="eTreeningud" url="https://etreeningud.ee"/>
-    <Layout landing>
-      <Landing />
-    </Layout>
+    <Head>
+        <title>eTreeningud</title>
+    </Head>
+
   </Fragment>
 }
 
