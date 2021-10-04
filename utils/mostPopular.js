3d43e7fc767977490cc8130d0511903e163b4aa5
Sten Olmre <60060439+stenolmre@users.noreply.github.com>
2021-10-04 17:31:24 +0300

    Update mostPopular.js

--- utils/mostPopular.js
+++ utils/mostPopular.js
@@ -1,18 +1,5 @@
 const mostPopular = (arr = []) => {
-  let item = arr[0]
-  let ocurrencesMap = {}
-
-  for (let i in arr) {
-    const current = arr[i]
-
-    if (ocurrencesMap[current]) ocurrencesMap[current]++
-    else ocurrencesMap[current] = 1
-
-    if (ocurrencesMap[item] < ocurrencesMap[current]) item = current
-  }
-
-//   return { item, ocurrences: ocurrencesMap[item] };
-  return item
+  return arr.sort((a, b) => arr.filter(element => element === a).length - arr.filter(element => element === b).length).pop()
 }
 
 export default mostPopular
