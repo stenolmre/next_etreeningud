a7fae17d16df33172ff7cc0eeaba4dff2ca795e1
Sten Olmre <60060439+stenolmre@users.noreply.github.com>
2021-10-04 16:59:05 +0300

    Create isValid.js

---
+++ utils/isValid.js
@@ -0,0 +1,33 @@
+import isEmail from '@utils/isEmail'
+
+const isValid = (setErrors, data) => {
+  let errors = {}
+  let is_valid = true
+  
+  Object.entries(data).forEach(([key, value]) => {
+    if (key === 'email' && !isEmail(value)) {
+      is_valid = false
+      errors[key] = `${key[0].toUpperCase() + key.slice(1).toLowerCase()} is required.`
+      return
+    }
+    
+    if (key === 'password' && value.length < 6) {
+      is_valid = false
+      errors[key] = `${key[0].toUpperCase() + key.slice(1).toLowerCase()} is required and must be longer than 6 chars.`
+      return
+    }
+    
+    if (value == null || value === '') {
+      is_valid = false
+      errors[key] = `${key[0].toUpperCase() + key.slice(1).toLowerCase()} is required.`
+      return
+    }
+    
+    return
+  })
+  
+  setErrors(errors)
+  return is_valid
+}
+
+export default isValid
