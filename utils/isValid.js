1a5ab78b40c90d906f4518daf42ab4db22720ac5
Sten Olmre <60060439+stenolmre@users.noreply.github.com>
2021-10-06 16:15:19 +0300

    Update isValid.js

--- utils/isValid.js
+++ utils/isValid.js
@@ -1,3 +1,4 @@
+import capitalize from '@utils/capitalize'
 import isEmail from '@utils/isEmail'
 import isURL from '@utils/isURL'
 import isImage from '@utils/isImage'
@@ -12,28 +13,28 @@ const isValid = (setErrors, data = {}, errorMessages = {}) => {
     if (key === 'email' && !isEmail(value)) {
       is_valid = false
       if (errorMessages.hasOwnProperty(key)) errors[key] = errorMessages[key]
-      else errors[key] = `${key[0].toUpperCase() + key.slice(1).toLowerCase()} is required.`
+      else errors[key] = `${capitalize(key)} is required.`
       return
     }
     
     if (key === 'password' && value.length < 6) {
       is_valid = false
       if (errorMessages.hasOwnProperty(key)) errors[key] = errorMessages[key]
-      else errors[key] = `${key[0].toUpperCase() + key.slice(1).toLowerCase()} is required and must be longer than 6 chars.`
+      else errors[key] = `${capitalize(key)} is required and must be longer than 6 chars.`
       return
     }
     
     if (key === 'image' && (!isURL(value) || !isImage(value))) {
       is_valid = false
       if (errorMessages.hasOwnProperty(key)) errors[key] = errorMessages[key]
-      else errors[key] = `${key[0].toUpperCase() + key.slice(1).toLowerCase()} is required and it must be URL with an ending of |jpeg jpg gif png|.`
+      else errors[key] = `${capitalize(key)} is required and it must be URL with an ending of |jpeg jpg gif png|.`
       return
     }
     
     if (value == null || value === '') {
       is_valid = false
       if (errorMessages.hasOwnProperty(key)) errors[key] = errorMessages[key]
-      else errors[key] = `${key[0].toUpperCase() + key.slice(1).toLowerCase()} is required.`
+      else errors[key] = `${capitalize(key)} is required.`
       return
     }
     
