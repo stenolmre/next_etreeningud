197768d6c4131459d625c472f7262952a2960b7c
Sten Olmre <60060439+stenolmre@users.noreply.github.com>
2021-10-06 16:30:15 +0300

    Update parseDate.js

--- utils/parseDate.js
+++ utils/parseDate.js
@@ -1,5 +1,5 @@
-const parseDate = (raw_date) => {
-  if (!raw_date) return
+const parseDate = raw_date => {
+  if (!raw_date) throw new Error('Pass in valid date to parse')
   const date = new Date(raw_date)
 
   return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
