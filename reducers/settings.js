3d1f79751eebfea0b095d48c7d3b539c47e80fbe
Sten Olmre <stenolmre@icloud.com>
2025-12-06 13:42:26 +0200

    update packages

--- reducers/settings.js
+++ reducers/settings.js
@@ -1,4 +1,8 @@
-import { UPDATE_SETTINGS, GET_SETTINGS, SETTINGS_ERROR } from './../actions/types'
+import {
+  UPDATE_SETTINGS,
+  GET_SETTINGS,
+  SETTINGS_ERROR,
+} from './../actions/types'
 
 export const initialState = {
   id: null,
@@ -12,7 +16,7 @@ export const initialState = {
   features: [],
   landing: null,
   loading: true,
-  error: null
+  error: null,
 }
 
 export const SettingsReducer = (state = initialState, action) => {
@@ -23,28 +27,28 @@ export const SettingsReducer = (state = initialState, action) => {
     case GET_SETTINGS:
       return {
         ...state,
-        id: payload[0]._id,
-        fit_exercises: payload[0].fit_exercises,
-        fit_categories: payload[0].fit_categories,
-        fit_equipment: payload[0].fit_equipment,
-        fit_images: payload[0].fit_images,
-        fit_ending_messages: payload[0].fit_ending_messages,
-        blog_categories: payload[0].blog_categories,
-        social: payload[0].social,
-        features: payload[0].features,
-        landing: payload[0].landing,
+        id: payload[0]?._id,
+        fit_exercises: payload[0]?.fit_exercises,
+        fit_categories: payload[0]?.fit_categories,
+        fit_equipment: payload[0]?.fit_equipment,
+        fit_images: payload[0]?.fit_images,
+        fit_ending_messages: payload[0]?.fit_ending_messages,
+        blog_categories: payload[0]?.blog_categories,
+        social: payload[0]?.social,
+        features: payload[0]?.features,
+        landing: payload[0]?.landing,
         loading: false,
-        error: null
+        error: null,
       }
     case SETTINGS_ERROR:
       return {
         ...state,
         loading: false,
-        error: payload
+        error: payload,
       }
     default:
       return {
-        state
+        state,
       }
   }
 }
